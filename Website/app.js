4093f4de9e369ee23422e6f5b9d582eab0875487
dcbentley <dcbentley@users.noreply.github.com>
2020-05-19 22:24:02 -0400

    Fix flexbox parent-child issues and added JS

---
+++ Website/app.js
@@ -0,0 +1,6 @@
+// Lets make a neat function
+function darkness() { // Naming of function
+    let element = document.body // Declaring variable and scope 
+    element.classList.toggle("lights"); // Toggle class .lights in CSS
+    console.log(document.body); // Console log check
+}
